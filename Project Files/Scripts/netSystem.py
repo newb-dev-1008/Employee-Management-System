7876cb52f2bea5989dd469b1bd9126f3f5f9b6e3
Yash Umale <yashumale223@gmail.com>
2021-11-14 19:29:13 +0530

    Recognizer function added

--- Project Files/Scripts/netSystem.py
+++ Project Files/Scripts/netSystem.py
@@ -43,6 +43,7 @@ class FaceRecognitionSystem:
         self.savePathRecognizer = savePathRecognizer
         self.savePathLabels = savePathLabels
 
+    # --------------------------------------- Program to train an SVM recognizer ---------------------------------------
     def trainRecognizer(self):
         # Argparse arguments
         savingMultiple = str(input("Saving multiple people?: ")) 
@@ -50,8 +51,6 @@ class FaceRecognitionSystem:
             name = # Name of person
         else:
             names = # List of names in order of photos
-
-        # --------------------------------------- Main Program ---------------------------------------
 
         # FaceNet
         protoPath = os.path.sep.join([self.protoPathFolder, 'deploy.prototxt'])
@@ -302,4 +301,53 @@ class FaceRecognitionSystem:
             f = open(self.savePathLabels, "wb")
             f.write(pickle.dumps(le))
             f.close()
+            
+    # ---------------------------------------------------------------------------------------------------------------------
 
+    # --------------------------------------- Program to recognize people real-time ---------------------------------------
+    def recognizePerson(self, imageURL):
+
+        # Initialize face detector
+        protoPath = os.path.sep.join([self.protoPathFolder, 'deploy.prototxt'])
+        modelPath = os.path.sep.join([self.modelPathFolder, 'res10_300x300_ssd_iter_140000.caffemodel'])
+        detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+        # Load our serialized face embedding model
+        embedder = cv2.dnn.readNetFromTorch(self.embedderPath)
+
+        # Load the SVM Model and LabelEncoder
+        recognizer = pickle.loads(open(self.savePathRecognizer, "rb").read())
+        le = pickle.loads(open(self.savePathLabels, "rb").read())
+
+        frame = # Use imageURL to 
+        frame = imutils.resize(frame, width = 600)
+        (h, w) = frame.shape[:2]
+
+        imageBlob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177, 123.0), swapRB = False, crop = False)
+        detector.setInput(imageBlob)
+            
+        detections = detector.forward()
+            
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if (confidence > 0.2):
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                
+                face = frame[startY: endY, startX: endX]
+                (fH, fW) = face.shape[:2]
+                if ((fW < 20) or (fH < 20)):
+                    continue
+                faceBlob = cv2.dnn.blobFromImage(face, 1.0/255, (96, 96), (0, 0, 0), swapRB = True, crop = False)
+                embedder.setInput(faceBlob)
+                vec = embedder.forward()
+                preds = recognizer.predict_proba(vec)[0]
+                j = np.argmax(preds)
+                proba = preds[j]
+                name = le.classes_[j]
+
+                returnStatus(None)
+            else:
+                returnStatus("Low confidence. Send a better photo.\n")
+
+    # -------------------------------------------------------------------------------------------
